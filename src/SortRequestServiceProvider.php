253fbe1bd02eeb72f952ad3463a352d3218afdea
musa11971 <mussesemou99@gmail.com>
2020-02-01 15:03:53 +0100

    📦 Initialized package

---
+++ src/SortRequestServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace musa11971\SortRequest;
+
+use Illuminate\Support\ServiceProvider;
+
+class SortRequestServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+
+    }
+}
