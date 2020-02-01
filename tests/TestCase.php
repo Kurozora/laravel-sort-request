d6dad104acded063ca6a27d06294c3ba29faf972
musa11971 <mussesemou99@gmail.com>
2020-02-01 15:25:46 +0100

    🧪 Added test suite

---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace musa11971\SortRequest\Tests;
+
+use \Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra {
+
+}
---
+++ tests/Unit/BasicTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace musa11971\SortRequest\Tests\Unit;
+
+use musa11971\SortRequest\Tests\TestCase;
+
+class BasicTest extends TestCase {
+    /** @test */
+    function it_can_multiply() {
+        $this->assertEquals(20, 4 * 5);
+    }
+}
